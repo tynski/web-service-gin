e08263fc8f8febb061d8d39b10353b8f32a55a6a
tynski <b.tynski@gmail.com>
2021-07-24 17:52:32 +0200

    album struct type fix

--- albums.go
+++ albums.go
@@ -11,7 +11,7 @@ type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	Price  string  `json:"price"`
 }
 
 var fileName = "albums.json"
